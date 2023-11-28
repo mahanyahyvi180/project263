75bef124d13a46235dcdd9bf095a900ae298f1c8
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-11-28 17:16:36 +0330

    Add files via upload

---
+++ WindowsFormsApp136/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp136
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] arr = System.Text.Encoding.UTF8.GetBytes(textBox1.Text);
+            System.IO.FileStream f = new System.IO.FileStream(@"G:\G-sharp-14020510\sample.txt", System.IO.FileMode.Create);
+            f.Write(arr, 0,arr.Length);
+            f.Flush();
+            f.Close();
+            string t = "abc";
+        }
+    }
+}
